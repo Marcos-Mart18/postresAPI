2cda4606cc79fe045ed99871d77d5bfe172b8bc5
Marquitoz <markst1811@gmail.com>
2025-09-27 20:43:17 -0500

    login response username

--- src/main/java/com/postres/service/Impl/AuthServiceImpl.java
+++ src/main/java/com/postres/service/Impl/AuthServiceImpl.java
@@ -79,6 +79,7 @@ public class AuthServiceImpl implements AuthService{
                 usuario.getApellidos(),
                 usuario.getCorreo(),
                 usuario.getProfileFotoUrl(),
+                usuario.getUsername(),
                 roles
         );
 
